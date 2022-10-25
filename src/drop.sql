c0619452e62b515c92ca2be2faab6cce86a7cd91
Dasxunya <dasha.ipad.mail@gmail.com>
2022-10-25 20:45:14 +0300

    drop tables💀

--- src/drop.sql
+++ src/drop.sql
@@ -1,3 +1,37 @@
 --Удаление всех таблиц БД
 
-DROP TABLE "";
+DROP TABLE "shop_type" CASCADE;
+
+DROP TABLE "address" CASCADE;
+
+DROP TABLE "shop" CASCADE;
+
+DROP TABLE "game_genre" CASCADE;
+
+DROP TABLE "game_theme" CASCADE;
+
+DROP TABLE "game_mechanics" CASCADE;
+
+DROP TABLE "game_publisher" CASCADE;
+
+DROP TABLE "board_game" CASCADE;
+
+DROP TABLE "game_to_theme" CASCADE;
+
+DROP TABLE "user_role" CASCADE;
+
+DROP TABLE "users" CASCADE;
+
+DROP TABLE "game_comment" CASCADE;
+
+DROP TABLE "game_to_shop" CASCADE;
+
+DROP TABLE "forum_topic" CASCADE;
+
+DROP TABLE "topic_comment" CASCADE;
+
+DROP TABLE "carts_of_users" CASCADE;
+
+DROP TABLE "favorites_of_users" CASCADE;
+
+DROP TABLE "users_fav_forums" CASCADE;
