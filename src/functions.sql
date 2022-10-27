28f9c46c1c83c38a77051d76e9c596e33521f51b
Dasxunya <dasha.ipad.mail@gmail.com>
2022-10-27 21:27:19 +0300

    function + trigger

--- src/functions.sql
+++ src/functions.sql
@@ -1,22 +1,9 @@
---TODO: Оно хотело бы жить, но пока не может
-CREATE OR REPLACE FUNCTION LovedForums() RETURNS TRIGGER AS
-$$
+create or replace function different_board_count() returns integer as $$
+declare
+    cnt integer;
 begin
-    if (select count(forum_id) from users_fav_forums group by user_id) > 10
-    then
-        delete
-        from users_fav_forums
-        where user_id = new.user_id
-          and forum_id = new.forum_id;
-        raise exception 'Превышено максимальное количество избранных форумов';
-    end if;
+    select count(id) into cnt from board_game;
+    return cnt;
 end;
-$$ LANGUAGE plpgsql;
+$$ language plpgsql;
 
---Триггер на максимальное количество любимых форумов (10)
-DROP TRIGGER IF EXISTS "CheckLovedForums" ON users_fav_forums;
-CREATE TRIGGER CheckLovedForums
-    AFTER INSERT
-    ON users_fav_forums
-    FOR EACH ROW
-EXECUTE PROCEDURE LovedForums();
